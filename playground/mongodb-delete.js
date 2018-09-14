277174986b296035660635704a033b9c93b6a4ff
Michael Gattas <mjgattas@gmail.com>
2018-09-13 20:48:17 -0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,39 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  // db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // deleteOne
+  // db.collection('Todos').deleteOne({ text: 'Eat lunch'} ).then((result) => {
+  //   console.log(result);
+  // });
+
+  // findOneAndDelete
+  // db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
+  //   console.log(result);
+  // });
+
+  // db.collection('Users').deleteMany({ name: 'Michael Gattas' }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndDelete({
+    _id: new ObjectID('5b9b06c7a04ce42aeed2280e')
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // db.close();
+});
